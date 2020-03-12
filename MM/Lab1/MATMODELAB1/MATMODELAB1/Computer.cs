67826212231867b7cc779c46190e052f3d4f246a
rexx.magtar <rexx.magtar@gmail.com>
2020-03-12 04:02:15 +0300

    MMLab1

---
+++ MM/Lab1/MATMODELAB1/MATMODELAB1/Computer.cs
@@ -0,0 +1,68 @@
+﻿using  System.Collections.Generic;
+using System;
+using System.Diagnostics;
+
+namespace MATMODELAB1
+{
+    public class Computer
+    {
+        private long a;
+        private long b;
+        private long M;
+
+        public Computer(int a, int b, int m)
+        {
+            this.a = a;
+            this.b = b;
+            M = m;
+        }
+
+        public static List<float> MKM(long a0, long b, long M,int size)
+        {
+            List<float> result =new List<float>();
+            long a=a0;
+            bool stop = false;
+
+            while (!stop)
+            {
+                a = (a * b) - M * ((a * b) / M);
+                result.Add((1.0f * a) / M);
+                if (result.Count == size)
+                {Debug.WriteLine("Finished");
+                    stop = true;
+                }
+            }
+
+            return result;
+        }
+
+        public static List<float> MMM(int K, int size, List<float> C , List<float> B)
+        {
+           
+            List<float> V =new List<float>();
+            List<float> result = new List<float>();
+            bool stop = false;
+            for (int i = 0; i < K; i++)
+            {
+                V.Add(B[i]);
+            }
+
+            while (!stop)
+            {
+            int s =(int)(C[result.Count] * K);
+                float a = V[s];
+                V[s] = B[result.Count+K];
+                result.Add(a);
+                if (result.Count == size)
+                {
+                    break;
+                }
+            }
+
+            return result;
+
+        }
+    }
+    
+
+}
